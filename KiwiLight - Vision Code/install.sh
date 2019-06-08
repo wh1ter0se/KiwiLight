8ec4fbf99131e6d6ac72282d916dbefebbff1fb6
BTK203 <brach2003@gmail.com>
2019-06-07 20:56:57 -0500

    wrote an installer

---
+++ KiwiLight - Vision Code/install.sh
@@ -0,0 +1,66 @@
+# ---------------------------------- #
+#                                    #
+#    UNIX INSTALLER FOR KIWILIGHT    #
+#                                    #
+# ---------------------------------- #
+
+Install() {
+    echo "Downloading Packages"
+
+    sudo apt-get --assume-yes install pkg-config
+    sudo apt-get --assume-yes install cmake
+    sudo apt-get --assume-yes install libgtk-3-dev
+    sudo apt-get --assume-yes install libv4l-dev
+    sudo apt-get --assume-yes install v4l-utils
+    
+    echo "Preparing to download OPENCV"
+    mkdir opencv
+    cd opencv
+
+    echo "downloading OPENCV"
+    wget https://github.com/opencv/opencv/archive/3.4.5.zip
+    mkdir src
+
+    echo "unzipping OPENCV"
+    unzip 3.4.5.zip -d src
+    mkdir build
+    cd build
+
+    echo "Preparing to compile OPENCV"
+    cmake -D CMAKE_BUILD_TYPE=RELEASE \
+          -D CMAKE_INSTALL_PREFIX=/usr/local \
+          -D WITH_GTK_2_X=FALSE \
+          -D WITH_GTK=TRUE \
+          -D BUILD_TESTS=FALSE \
+          ../src/opencv-3.4.5
+
+    echo "Compiling OPENCV (this will take a while!)"
+    sudo make -j2 install
+    sudo ldconfig
+
+    #return back to normal directory
+    cd ../..
+
+    echo "Preparing to compile KIWILIGHT"
+    mkdir bin
+    mkdir lib
+
+    echo "Compiling KIWILIGHT"
+    make -j2 KiwiLight
+
+    echo "KIWILIGHT has been successfully installed."
+}
+
+
+echo "Welcome to the KiwiLight installer!"
+echo "Installing KiwiLight will download many packages and may take a long time."
+echo "Would you like to continue? [y/n]: "
+
+read -n 1 confirm
+echo
+
+if [ ${confirm} = 'y' ]; then
+    Install
+    else
+    echo "Aborting."
+fi
